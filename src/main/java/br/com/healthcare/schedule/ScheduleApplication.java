001ee01dfa3612a8cd109a0734b8727bb5098121
Antonio <pisouza011@gmail.com>
2024-06-24 22:20:57 -0300

    feat: Start Project First Commit

---
+++ src/main/java/br/com/healthcare/schedule/ScheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.healthcare.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/healthcare/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.healthcare.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
